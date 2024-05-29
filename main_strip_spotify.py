ad81899a4d2e0012a4f93e6e1360977aec8c8f42
bedakcool <153218579+raziskovalnaleon@users.noreply.github.com>
2024-05-29 14:13:43 +0200

    Update main_strip_spotify.py
    crucial changes

--- main_strip_spotify.py
+++ main_strip_spotify.py
@@ -8,17 +8,17 @@ from youtubesearchpython import Search
 
 
 
-CLIENT_ID = 'your_client_id'
-CLIENT_SECRET = 'your_secret'
+CLIENT_ID = 'client_id'
+CLIENT_SECRET = 'client_secret'
 # Here at PLAYLIST_LINK you can put whatever spotify link of album you want. WARNING: it will strip 100 songs per round
-PLAYLIST_LINK = "https://open.spotify.com/playlist/2myW2EQLU9T5h6s6hKvOid?si=e72ff2584e6045cb" 
+# PLAYLIST_LINK = "https://open.spotify.com/playlist/3vP9BQxraLqDolt4wuiB0C?si=d5d53b7325944827" # rock 1 --> 100 songs 
+PLAYLIST_LINK = "https://open.spotify.com/playlist/2myW2EQLU9T5h6s6hKvOid?si=e72ff2584e6045cb" # rock 2 --> 47 songs
 PLAYLIST_URI = PLAYLIST_LINK.split("/")[-1].split("?")[0]
 
 #Authentication - without user
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 myclient = Client(CLIENT_ID, CLIENT_SECRET)
-
 
 final_track_list = list()
 
@@ -27,12 +27,15 @@ print(f"{len_for_precent} songs on this album")
 # Fetching all the songs names, youtube links 
 for x,track in enumerate(sp.playlist_tracks(PLAYLIST_URI)["items"]):
     track_name = track["track"]["name"]
+    track_uri = track["track"]["uri"]
+    artist_name = track["track"]["artists"][0]["name"]
+
     track = myclient.search(track_name).get_tracks()[0]
-    search_word = f"{track.artists[0].name}{track.name}"
+    search_word = f"{artist_name}{track.name}"
     allSearch = Search(search_word, limit = 1)
     # print(track)
     # print(f"{track_uri}---{track_name}---{artist_uri}---{artist_name}")
-    final_track_list.append(f"{track.name},{track.artists[0].name},{track.url},{allSearch.result()['result'][0]['link']}")
+    final_track_list.append(f"{track_name},{artist_name},{track_uri},{allSearch.result()['result'][0]['link']}")
     precent_for_show = ((x + 1) / len_for_precent) * 100
     print(f"{int(precent_for_show)}% completed")
 # Writing data int .txt file

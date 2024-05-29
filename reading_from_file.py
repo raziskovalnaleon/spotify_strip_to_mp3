bca322366a51a58c02428c8d29a8fcdf857dd0a1
bedakcool <153218579+raziskovalnaleon@users.noreply.github.com>
2024-05-29 13:24:20 +0200

    Update reading_from_file.py
    just uncommented some code

--- reading_from_file.py
+++ reading_from_file.py
@@ -13,14 +13,14 @@ for item in final_track_list:
 for idlink, link in enumerate(downloadable_url_list):
     print(f"{idlink + 1}:{link}")
     # test_link = link
-    # yt = YouTube(link)
-    # video = yt.streams.filter(only_audio=True).first()
-    # destination = './songs/'
-    # out_file = video.download(output_path=destination)  # file saves as 'mp4'
-    # base, ext = os.path.splitext(out_file)  # 'cuts' the mp4 part
-    # new_file = base + '.mp3'  # creates a file with mp3 end
-    # os.rename(out_file, new_file)  # and it renames it
-    # print(yt.title + " has been successfully downloaded.")
+    yt = YouTube(link)
+    video = yt.streams.filter(only_audio=True).first()
+    destination = './songs/' # set destination whereever you want
+    out_file = video.download(output_path=destination)  # file saves as 'mp4'
+    base, ext = os.path.splitext(out_file)  # 'cuts' the mp4 part
+    new_file = base + '.mp3'  # creates a file with mp3 end
+    os.rename(out_file, new_file)  # and it renames it
+    print(yt.title + " has been successfully downloaded.")
 
 
 file_links.close()
